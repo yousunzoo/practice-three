f60ed1fa09b6333e647ec45779e90994286c0b60
yousunzoo <dpfvmffjqm@gmail.com>
2024-10-13 20:28:22 +0900

    feat: Floor 컴포넌트 세팅

---
+++ src/entities/basic/ui/Floor.tsx
@@ -0,0 +1,8 @@
+export const Floor = () => {
+    return (
+      <mesh rotation-x={-Math.PI / 2} receiveShadow>
+        <circleGeometry args={[10]} />
+        <meshStandardMaterial />
+      </mesh>
+    )
+  }
